8f71bb0c33dbb2cc282073f003cf8954ea86eb3c
Filip Topalovski <55166162+ftopalovski@users.noreply.github.com>
2024-09-19 10:39:31 -0400

    add General

---
+++ src/components/General.jsx
@@ -0,0 +1,18 @@
+function General () {
+    const name = "Filip Topalovski";
+    const email = "ftopalovski@gmail.com";
+    const phone = "905-746-5088"
+
+    return <>
+        <div>
+            <h1>General Information</h1>
+            <ul>
+                <li>Name: {name}</li>
+                <li>E-mail: {email}</li>
+                <li>Phone: {phone}</li>
+            </ul>
+        </div>
+    </>
+}
+
+export default General;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import General from './components/General.jsx'
 import './index.css'
+import { formatPostcssSourceMap } from 'vite'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <General />
   </StrictMode>,
 )
